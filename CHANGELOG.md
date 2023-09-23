de7d64b90b96a8d5f48c9d20d0adbe48ae26ce8d
Serkan UNCU <serkanuncuu@gmail.com>
2023-09-23 14:21:44 +0300

    Add new icon. Author name and email information has been asked at the beginning. It has been ensured that it works in accordance with various templates. Some bugs fixed and improved.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,7 +4,14 @@ All notable changes to the "auto-header-vscode" extension will be documented in 
 
 ## [Unreleased]
 
-- Fix the marketplace image.
+- Compatible with various languages
+
+## [1.0.2] - 2023-09-23
+
+- Added new icon and marketplace image.
+- Author name and email information has been asked at the beginning.
+- It has been ensured that it works in accordance with various templates.
+- Some bugs fixed and improved.
 
 ## [1.0.1] - 2023-09-20
 
--- src/extension.ts
+++ src/extension.ts
@@ -2,74 +2,128 @@
  * @file: extension.ts
  * @author: Serkan UNCU <serkanuncuu@gmail.com>
  * Date: 20.09.2023
- * Last Modified Date: 20.09.2023
+ * Last Modified Date: 23.09.2023
  * Last Modified By: Serkan UNCU <serkanuncuu@gmail.com>
  */
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-	const config = vscode.workspace.getConfiguration('headerComment');
-    const authorName = config.get<string>('authorName');
-    const authorEmail = config.get<string>('authorEmail');
 
-	const formatDate = (date: Date): string => {
-		const day = String(date.getDate()).padStart(2, '0');
-		const month = String(date.getMonth() + 1).padStart(2, '0');
-		const year = date.getFullYear();
-	
-		return `${day}.${month}.${year}`;
-	};
-	
+  const askForName = async () => {
+    return await vscode.window.showInputBox({ prompt: 'Please enter your name:' });
+  };
 
-    const addOrUpdateHeaderComment = (document: vscode.TextDocument) => {
-        const content = document.getText();
-        const headerPattern = /\/\*\*\s*\n \* @file: .+\s*\n \* @author: .+<.+>\s*\n \* Date: \d{2}\.\d{2}\.\d{4}\s*\n \* Last Modified Date: \d{2}\.\d{2}\.\d{4}\s*\n \* Last Modified By: .+<.+>\s*\n \*\//;
+  const askForEmail = async () => {
+    return await vscode.window.showInputBox({ prompt: 'Please enter your email:' });
+  };
 
-        // If exists comment lines then update comments
-        if (content.match(headerPattern)) {
-			const lastModifiedDatePattern = /Last Modified Date: \d{2}\.\d{2}\.\d{4}/;
-			const newLastModifiedDate = `Last Modified Date: ${formatDate(new Date())}`;
-	
-			const lastModifiedByPattern = /Last Modified By: .+<.+>/;
-			const newLastModifiedBy = `Last Modified By: ${authorName} <${authorEmail}>`;
+  const isAuthorNameSet = vscode.workspace.getConfiguration().get('headerComment.authorName');
+  const isAuthorEmailSet = vscode.workspace.getConfiguration().get('headerComment.authorEmail');
 
-			const edit = new vscode.WorkspaceEdit();
+  if (!isAuthorNameSet || !isAuthorEmailSet) {
+    askForName().then((name) => {
+      if (name) {
+        askForEmail().then((email) => {
+          if (email) {
+            const config = vscode.workspace.getConfiguration('headerComment');
+            config.update('authorName', name, vscode.ConfigurationTarget.Global);
+            config.update('authorEmail', email, vscode.ConfigurationTarget.Global);
+          }
+        });
+      }
+    });
+  }
 
-            if (content.includes('Last Modified Date:') && content.match(lastModifiedDatePattern)) {
-				const range = new vscode.Range(document.positionAt(content.search(lastModifiedDatePattern)), document.positionAt(content.search(lastModifiedDatePattern) + newLastModifiedDate.length));
-				edit.replace(document.uri, range, newLastModifiedDate);
-			}
-	
-			if (content.includes('Last Modified By:') && content.match(lastModifiedByPattern)) {
-				const range = new vscode.Range(document.positionAt(content.search(lastModifiedByPattern)), document.positionAt(content.search(lastModifiedByPattern) + newLastModifiedBy.length));
-				edit.replace(document.uri, range, newLastModifiedBy);
-			}
+  const config = vscode.workspace.getConfiguration('headerComment');
+  const authorName = config.get<string>('authorName');
+  const authorEmail = config.get<string>('authorEmail');
 
-			vscode.workspace.applyEdit(edit);
-        }	else {
-			// If not exists comment lines then generate comment lines
-            const newHeaderContent = `/**
- * @file: ${document.fileName.split('/').pop()}
+  if (!authorName || !authorEmail) {
+    vscode.window.showWarningMessage('Please set author name and email in settings before using this feature. Do you want set now?',
+    'Yes').then(selection => {
+      if (selection === 'Yes') {
+        vscode.commands.executeCommand('workbench.action.openSettings');
+      }
+    });
+  }
+
+  const formatDate = (date: Date): string => {
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+
+    return `${day}.${month}.${year}`;
+  };
+
+  const addOrUpdateHeaderComment = (document: vscode.TextDocument) => {
+    const content = document.getText();
+    const headerPattern = /\/\*\*[\s\S]*?@file\s*.*[\s\S]*?@author\s*.*[\s\S]*?\*\//;
+    const match = headerPattern.exec(content);
+
+    const edit = new vscode.WorkspaceEdit();
+    // If exists comment lines then update comments
+    if (match) {
+      const headerContent = match[0];
+      const headerEnd = match.index + headerContent.length - 3;
+      const headerLines = headerContent.split('\n');
+      const headerStart = match.index;
+
+      if (headerLines.length <= 5) {
+        const startPosition = document.positionAt(headerEnd);
+        const endPosition = document.positionAt(headerEnd + 3);
+
+        const replaceRange = new vscode.Range(startPosition, endPosition);
+
+        const newContent = ` * Last Modified Date: ${formatDate(new Date())}\n * Last Modified By: ${authorName} <${authorEmail}>\n */`;
+
+        edit.replace(document.uri, replaceRange, newContent);
+      } else if (headerLines.length === 6) {
+        const newContent = ` * Last Modified Date: ${formatDate(new Date())}\n * Last Modified By: ${authorName} <${authorEmail}>`;
+
+        const changingLineStartPosition = document.positionAt(headerStart + headerContent.indexOf(headerLines[4]));
+        const changingLineEndPosition = document.positionAt(headerStart + headerContent.indexOf(headerLines[4]) + headerLines[4].length);
+        const changingLineRange = new vscode.Range(changingLineStartPosition, changingLineEndPosition);
+
+        edit.replace(document.uri, changingLineRange, newContent);
+      } else {
+        const newLastModifiedDate = ` * Last Modified Date: ${formatDate(new Date())}`;
+        const newLastModifiedBy = ` * Last Modified By: ${authorName} <${authorEmail}>`;
+
+        const lastModifiedDateStartPosition = document.positionAt(headerStart + headerContent.indexOf(headerLines[4]));
+        const lastModifiedDateEndPosition = document.positionAt(headerStart + headerContent.indexOf(headerLines[4]) + headerLines[4].length);
+        const lastModifiedDateRange = new vscode.Range(lastModifiedDateStartPosition, lastModifiedDateEndPosition);
+
+        const lastModifiedByStartPosition = document.positionAt(headerStart + headerContent.indexOf(headerLines[5]));
+        const lastModifiedByEndPosition = document.positionAt(headerStart + headerContent.indexOf(headerLines[5]) + headerLines[5].length);
+        const lastModifiedByRange = new vscode.Range(lastModifiedByStartPosition, lastModifiedByEndPosition);
+
+        edit.replace(document.uri, lastModifiedDateRange, newLastModifiedDate);
+        edit.replace(document.uri, lastModifiedByRange, newLastModifiedBy);
+      }
+    } else {
+      // If not exists comment lines then generate comment lines
+      const relativePath = vscode.workspace.asRelativePath(document.uri);
+      const newHeaderContent = `/**
+ * @file: ${relativePath}
  * @author: ${authorName} <${authorEmail}>
  * Date: ${formatDate(new Date())}
  * Last Modified Date: ${formatDate(new Date())}
  * Last Modified By: ${authorName} <${authorEmail}>
  */
 ${content}`;
+      const range = new vscode.Range(new vscode.Position(0, 0), new vscode.Position(document.lineCount, 0));
+      edit.replace(document.uri, range, newHeaderContent);
+    }
 
-            const edit = new vscode.WorkspaceEdit();
-            const range = new vscode.Range(new vscode.Position(0, 0), new vscode.Position(document.lineCount, 0));
-            edit.replace(document.uri, range, newHeaderContent);
-            vscode.workspace.applyEdit(edit);
-        }
-    };
+    vscode.workspace.applyEdit(edit);
+  };
 
-    context.subscriptions.push(vscode.commands.registerCommand('auto-header-vscode.addHeaderComment', () => {
-        const document = vscode.window.activeTextEditor?.document;
-        if (!document) {
-            vscode.window.showWarningMessage('At least one file must be open!');
-            return;
-        }
-        addOrUpdateHeaderComment(document);
-    }));
+  context.subscriptions.push(vscode.commands.registerCommand('auto-header-vscode.addHeaderComment', () => {
+    const document = vscode.window.activeTextEditor?.document;
+    if (!document) {
+      vscode.window.showWarningMessage('At least one file must be open!');
+      return;
+    }
+    addOrUpdateHeaderComment(document);
+  }));
 }
